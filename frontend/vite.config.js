0155aa474b8506dafafca8943cb716121e6f9c6d
Balamurugan <139839683+BalamuruganRamachandran-Techfinite@users.noreply.github.com>
2026-05-27 11:14:00 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -18,8 +18,8 @@ export default defineConfig(async ({ mode }) => {
         },
         manifest: {
           display: 'standalone',
-          name: 'Frappe CRM',
-          short_name: 'Frappe CRM',
+          name: 'CRM',
+          short_name: 'CRM',
           start_url: '/crm',
           description:
             'Modern & 100% Open-source CRM tool to supercharge your sales operations',
